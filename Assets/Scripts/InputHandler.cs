21a52cf3ec3004298882d46335ab3ca0dabe9ae8
Alrick VINCENT <alrick.vincent@gmail.com>
2025-02-27 14:39:03 -0800

    Player Movement + Player & Ground Friction

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputHandler : MonoBehaviour
+{
+    [Header("InputEvents")]
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnSpacePressed = new UnityEvent();
+    public UnityEvent OnLeftClick = new UnityEvent();
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = Vector2.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveInput += Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            moveInput -= Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveInput += Vector2.left;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            moveInput -= Vector2.left;
+        }
+
+        OnMove.Invoke(moveInput.normalized);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private InputHandler inputHandler;
+    [SerializeField] public float moveSpeed = 1f;
+    private Rigidbody rb;
+    private Transform cameraTransform;
+
+    void Awake()
+    {
+        inputHandler = GetComponent<InputHandler>();
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    void Start()
+    {
+
+        cameraTransform = Camera.main.transform;
+
+        inputHandler.OnMove.AddListener(Move);
+
+        
+    }
+
+    private void Move(Vector2 input)
+    {
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+        forward.y = 0; right.y = 0;
+        forward.Normalize(); right.Normalize();
+
+        Vector3 moveDirection = (forward * input.y  + right * input.x).normalized;
+        rb.AddForce(moveDirection * moveSpeed, ForceMode.Acceleration);
+    }
+}
